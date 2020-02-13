0f9f549e1a85f9e35e4ea5ae5bd57aae3cdb0561
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2020-02-13 12:09:30 +0100

    Add material ui.

---
+++ src/components/menu.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {Button, Menu, MenuItem} from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+export function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Open Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export function MainBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Sorting visualization
+          </Typography>
+          <SimpleMenu />
+          <Button color="inherit">Sort</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,22 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {MainBar} from "./components/menu";
 
-ReactDOM.render(<h1>Hello world from react.</h1>, document.getElementById("root"));
+async function run() {
+    const wasm = await import("../sorting_rust/pkg/sorting");
+
+    let arr = [2,1,5,4,3]
+    let arri32 = Int32Array.from(arr);
+    //let animations = wasm.bouble_sort(arr);
+    let array = arr.map( (n, _idx) => <li>{ n.toString() }</li> );
+    ReactDOM.render(
+        <div>
+            <MainBar />
+            <ul>
+                { array }
+            </ul>
+        </div>
+        , document.getElementById("root"));
+}
+
+run()
