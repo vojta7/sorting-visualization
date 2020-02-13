7622e8627f4cba9695dab5891e8ab707c66f274a
Vojtěch Pejša <vojtechpejsa7@gmail.com>
2020-02-13 13:19:40 +0100

    Menu mockup.

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,52 +1,64 @@
 import React from 'react';
-import {Button, Menu, MenuItem} from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {Button, MenuItem} from '@material-ui/core';
+import { createStyles, makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Select from '@material-ui/core/Select';
 
-export function SimpleMenu() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+enum Alghoritm {
+    Bouble,
+    Quick
+}
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
+const alghoritmUseStyles = makeStyles(
+  createStyles({
+    label: {
+        "margin-right": 20,
+        "vertical-align": "super",
+    },
+  }),
+);
+
+function CustomizedSelects() {
+  const classes = alghoritmUseStyles();
+  const [alghoritm, setAlghoritm] = React.useState('');
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setAlghoritm(event.target.value as string);
   };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
-    <div>
-      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        Open Menu
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
+      <div>
+        <span className={classes.label}>Select alghoritm</span>
+        <Select
+          labelId="alghoritm-select-label"
+          id="alghoritm-select"
+          value={alghoritm}
+          onChange={handleChange}
+        >
+          <MenuItem value={Alghoritm.Bouble}>Bouble Sort</MenuItem>
+          <MenuItem value={Alghoritm.Quick}>Quick Sort</MenuItem>
+        </Select>
     </div>
   );
 }
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(
   createStyles({
     root: {
       flexGrow: 1,
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
     title: {
+      flexGrow: 2,
+    },
+    alghoritm: {
+      flexGrow: 2,
+    },
+    sort: {
       flexGrow: 1,
     },
+    randomize: {
+      flexGrow: 1,
+    }
   }),
 );
 
@@ -60,8 +72,11 @@ export function MainBar() {
           <Typography variant="h6" className={classes.title}>
             Sorting visualization
           </Typography>
-          <SimpleMenu />
-          <Button color="inherit">Sort</Button>
+          <div className={classes.alghoritm}>
+              <CustomizedSelects />
+          </div>
+          <Button color="inherit" className={classes.sort}>Sort</Button>
+          <Button color="inherit" className={classes.randomize}>Randomize</Button>
         </Toolbar>
       </AppBar>
     </div>
